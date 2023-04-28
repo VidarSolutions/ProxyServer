3b14778835a3ac69d1ff6fac64f82573b9d570a7
Jeff <jeff@fgeth.com>
2023-04-28 13:33:34 -0500

    a different approach

--- proxyServer.go
+++ proxyServer.go
@@ -10,60 +10,61 @@ import (
 	"strconv"
 	"strings"
 	"golang.org/x/net/html"
+	"github.com/vidarsolutions/Transfer"
 )
 
 func ProxyServer(target string, res http.ResponseWriter, req *http.Request) {
-	proxy := &httputil.ReverseProxy{
-		Director: func(req *http.Request) {
-			req.URL.Scheme = "http"
-			req.URL.Host = target
-			req.Host = "" // Remove Host header to prevent redirects
-		},
-		ModifyResponse: func(resp *http.Response) error {
-			if resp.Header.Get("Content-Type") == "text/html" {
-				doc, err := html.Parse(resp.Body)
-				if err != nil {
-					return err
-				}
-				resp.Body.Close()
+	var t =Transfer.Dialer("127.0.0.1:9050")
+		if err != nil {
+			fmt.Println("Error creating request: ", err)
+			return
+		}
 
-				var traverse func(*html.Node)
-				traverse = func(n *html.Node) {
-					if n.Type == html.ElementNode {
-						if n.Data == "a" {
-							for i, attr := range n.Attr {
-								if attr.Key == "href" {
-									// Rewrite the link to point back to the onion site
-									n.Attr[i].Val = strings.ReplaceAll(attr.Val, "http://"+target, req.URL.String())
-								}
-							}
-						} else if n.Data == "img" {
-							for i, attr := range n.Attr {
-								if attr.Key == "src" {
-									// Rewrite the image URL to point back to the onion site
-									n.Attr[i].Val = strings.ReplaceAll(attr.Val, "http://"+target, req.URL.String())
-								}
-							}
+	resp, _ = t.Request("GET", target, nil)
+		if err != nil {
+			fmt.Println("Error fetching response: ", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body: ", err)
+			return
+		}
+
+		doc, err := html.Parse(strings.NewReader(string(body)))
+		if err != nil {
+			fmt.Println("Error parsing HTML: ", err)
+			return
+		}
+
+		var f func(*html.Node)
+		f = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "a" {
+				for i, a := range n.Attr {
+					if a.Key == "href" {
+						u, err := url.Parse(a.Val)
+						if err != nil {
+							continue
+						}
+						if u.Host == target.Host {
+							u.Host = req.Host
+							u.Scheme = req.URL.Scheme
+							n.Attr[i].Val = u.String()
 						}
 					}
-					for c := n.FirstChild; c != nil; c = c.NextSibling {
-						traverse(c)
-					}
 				}
-				traverse(doc)
-
-				var buf bytes.Buffer
-				if err = html.Render(&buf, doc); err != nil {
-					return err
-				}
-				resp.Body = ioutil.NopCloser(&buf)
-				resp.Header.Set("Content-Length", strconv.Itoa(buf.Len()))
 			}
-			return nil
-		},
-	}
 
-	proxy.ServeHTTP(res, req)
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+
+		f(doc)
+
+		html.Render(res, doc)
 }
 
 
